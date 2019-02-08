9a4e171066567f189ee47bfbabdde2ea709fdbdc
shivangi <shivangi.das7@gmail.com>
2019-02-09 02:28:48 +0530

    Updates

---
+++ precache-manifest.be347057de87e98764b232c5472f0bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31963bdd32f5110fe4df",
+    "url": "/movie/static/js/runtime~main.31963bdd.js"
+  },
+  {
+    "revision": "ee025dd14b639e1b8a90",
+    "url": "/movie/static/js/main.ee025dd1.chunk.js"
+  },
+  {
+    "revision": "63304ddfa1a2c2c53c9b",
+    "url": "/movie/static/js/1.63304ddf.chunk.js"
+  },
+  {
+    "revision": "ee025dd14b639e1b8a90",
+    "url": "/movie/static/css/main.3e075c6b.chunk.css"
+  },
+  {
+    "revision": "63304ddfa1a2c2c53c9b",
+    "url": "/movie/static/css/1.9cb9bda8.chunk.css"
+  },
+  {
+    "revision": "4ec9e234944e733b1e10cc594499ef88",
+    "url": "/movie/index.html"
+  }
+];
